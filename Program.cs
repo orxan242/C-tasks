faf3cab60fa72408fa25c709f971bc0f6fcb1fd7
orxan242 <orkhanka@code.edu.az>
2024-10-23 12:00:19 +0400

    added new tasks

---
+++ Program.cs
@@ -0,0 +1,123 @@
+﻿#region Task1
+//string name = "baku city";
+//string sum = name.Replace(" ", " ");
+//Console.WriteLine("Orijinal Yazı: \"" + name + "\"");
+//Console.WriteLine("Boşluqlar Silinmiş Yazı: \"" + sum + "\"");
+#endregion
+
+#region Task2
+//string name = "map";
+//char letter = 'a';
+//int index = -1;
+//for (int i = 0; i < name.Length; i++)
+//{
+//    if (name[i] == letter)
+//        index = i;
+//    break;
+//}
+//if (index != -1)
+//{
+//    Console.WriteLine("char sozun icinde deyil");
+//}
+//else
+//{
+//    Console.WriteLine("car sozun icindedir");
+//}
+#endregion
+
+#region Task3
+//string name = "  salam";
+//string sum = name.TrimStart();
+//Console.WriteLine("Orijinal Yazı: \"" + name + "\"");
+//Console.WriteLine("bosluqlari silinmis yazi: \"" + sum + "\"");
+
+#endregion
+
+#region Tas4
+//int x = 8, y = 2;
+//int result;
+
+//result = (x + y);
+//Console.WriteLine("Addition Operator: " + result);
+
+//result = (x - y);
+//Console.WriteLine("Subtraction Operator: " + result);
+
+//result = (x * y);
+//Console.WriteLine("Multiplication Operator: " + result);
+
+//result = (x / y);
+//Console.WriteLine("Division Operator: " + result);
+
+//result = (x % y);
+//Console.WriteLine("Modulo Operator: " + result);
+#endregion
+
+#region Task5
+//string name = "task";
+//char letter = 'a';
+//int index = -1;
+//if (index != -1)
+//{
+//    Console.WriteLine("yoxdur");
+//}
+//else
+//{
+//    Console.WriteLine("var");
+//}
+
+
+
+
+
+#endregion
+
+#region Task6
+//string name = "addtask";
+//int count = 0;
+//foreach (char c in name)
+//{
+//    if (c == 'a')
+//    {
+//        count++;
+//    }
+//}
+//Console.WriteLine(count);
+#endregion
+
+#region Task7
+//int num = 12;
+//while (num > 12) ;
+//if (num > 0 && num % 2 == 0)
+//{
+//    Console.WriteLine("musbet ve cut");  
+//}
+//else
+//{
+//    Console.WriteLine("duzgun eded daxil et");
+//}
+
+#endregion
+
+#region Task8
+//int tehsil = 100;
+//switch (tehsil)
+//{
+//    case 400:
+//        Console.WriteLine("programming");
+//        break;
+
+//    case 250:
+//        Console.WriteLine("design");
+//        break;
+
+//    case 200:
+//        Console.WriteLine("system");
+//        break;
+
+//    default:
+//        Console.WriteLine("tehsil novu yalnisdir");
+//        break;
+//}
+
+#endregion
